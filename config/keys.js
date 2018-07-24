b7235e6c2f29f5aa780f6a27866fa85b28900f8d
Valentin Beutler <valentin.beutler@hotmail.com>
2018-07-24 22:59:56 +0200

    Finished Section 3, Session 12

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://vbeutler:vbeutler_30@ds247141.mlab.com:47141/devconnector_val"
+    "mongodb://vbeutler:vbeutler_30@ds247141.mlab.com:47141/devconnector_val",
+  secretOrKey: "secret"
 };
