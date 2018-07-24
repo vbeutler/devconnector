77161a9dd8842dfc43e2cc1fbc3a653e6cfbeb8d
Valentin Beutler <valentin.beutler@hotmail.com>
2018-07-24 21:40:31 +0200

    Finished Section 3, Session 10

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/users/test
-// @desc    Tests user route
+// @route   GET api/profile/test
+// @desc    Tests profile route
 // @access  Public
 
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
